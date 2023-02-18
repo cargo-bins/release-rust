23fc404a7eedd75641e219195bd47c65aa0362a1
Félix Saparelli <felix@passcod.name>
2023-02-18 21:05:30 +1300

    Implement release phase (except release-separately)

--- src/phases/release.ts
+++ src/phases/release.ts
@@ -1,12 +1,16 @@
+import {basename, join} from 'node:path';
+import {debug, error, info, isDebug, warning} from '@actions/core';
+import {getOctokit} from '@actions/github';
+import {Minimatch} from 'minimatch';
+
+import {Pattern, PatternMap} from '../schemata/common';
+import {PatternList} from '../common/pattern-list';
 import {runHook} from '../common/exec';
+import {glob} from '../common/glob';
 import {InputsType} from '../schemata/index';
 import {CargoPackage} from '../cargo/metadata';
-import {TaggedRelease} from './tag';
 import {renderTemplate} from '../common/template';
-import {Minimatch} from 'minimatch';
-import {debug, isDebug, warning} from '@actions/core';
-import {Pattern, PatternMap} from '../schemata/common';
-import { PatternList } from '../common/pattern-list';
+import {TaggedRelease} from './tag';
 
 export default async function releasePhase(
 	inputs: InputsType,
@@ -22,27 +26,8 @@ export default async function releasePhase(
 				throw new Error('No tag for release');
 			}
 
-			const vars = {
-				target: inputs.setup.target,
-				'crate-name': release.name,
-				'crate-version': release.version,
-				'release-name': release.name,
-				'release-version': release.version,
-				'release-tag': tag.tagName
-			};
-			const releaseName = renderTemplate(
-				mapPatternFor(inputs.release.name, release.name) ??
-					'{crate-version}',
-				vars
-			);
-			const releaseBody = renderTemplate(
-				mapPatternFor(inputs.release.notes, release.name) ?? '',
-				{...vars, 'release-name': releaseName}
-			);
-			const markLatest = isLatest(inputs.release.latest, release.name);
-			const markPre = isPre(inputs.release.pre, release.name);
-			// TODO: create release if not exists
-			// TODO: upload assets
+			const releaseId = await ensureReleaseExists(inputs, tag, release);
+			await uploadAssets(inputs, releaseId);
 		}
 	}
 
@@ -81,4 +66,178 @@ function isPre(input: boolean | Pattern[], crateName: string): boolean {
 	}
 
 	return new PatternList(input).matchOne(crateName);
+}
+
+async function ensureReleaseExists(
+	inputs: InputsType,
+	tag: TaggedRelease,
+	release: CargoPackage
+): Promise<number> {
+	const extantRelease = await releaseId(
+		inputs.credentials.github,
+		tag.tagName
+	);
+	if (extantRelease) {
+		info(`Release ${tag.tagName} already exists (ID=${extantRelease})`);
+		return extantRelease;
+	} else {
+		debug(`Figuring out release details for ${tag.tagName}`);
+
+		const vars = {
+			target: inputs.setup.target,
+			'crate-name': tag.crate.name,
+			'crate-version': tag.crate.version,
+			'release-name': release.name,
+			'release-version': release.version,
+			'release-tag': tag.tagName
+		};
+		const releaseName = renderTemplate(
+			mapPatternFor(inputs.release.name, tag.crate.name) ??
+				'{crate-version}',
+			vars
+		);
+		const releaseBody = renderTemplate(
+			mapPatternFor(inputs.release.notes, tag.crate.name) ?? '',
+			{...vars, 'release-name': releaseName}
+		);
+		const markLatest = isLatest(inputs.release.latest, tag.crate.name);
+		const markPre = isPre(inputs.release.pre, tag.crate.name);
+
+		try {
+			const newRelease = await createRelease(
+				inputs.credentials.github,
+				tag.tagName,
+				releaseName,
+				releaseBody,
+				markPre,
+				markLatest
+			);
+			info(`Created release ${tag.tagName} (ID=${newRelease})`);
+			return newRelease;
+		} catch (err) {
+			debug(
+				'Creating failed, checking if release was created while we were trying'
+			);
+			const extantRelease = await releaseId(
+				inputs.credentials.github,
+				tag.tagName
+			);
+			if (extantRelease) {
+				info(
+					`Release ${tag.tagName} already exists (ID=${extantRelease})`
+				);
+				return extantRelease;
+			}
+
+			error(`Failed to create release ${tag.tagName}: ${err}`);
+			throw err;
+		}
+	}
+}
+
+async function releaseId(
+	github: ReturnType<typeof getOctokit>,
+	tag: string
+): Promise<number | null> {
+	const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/');
+
+	try {
+		debug(`Checking if release ${tag} exists`);
+		const {data, status} = await github.rest.repos.getReleaseByTag({
+			owner,
+			repo,
+			tag
+		});
+
+		if (status !== 200) {
+			return null;
+		}
+
+		return data.id;
+	} catch (err: unknown) {
+		return null;
+	}
+}
+
+async function createRelease(
+	github: ReturnType<typeof getOctokit>,
+	tag: string,
+	name: string,
+	body: string,
+	prerelease: boolean,
+	latest: boolean
+): Promise<number> {
+	const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/');
+
+	debug(
+		`Creating release ${tag}: ${JSON.stringify({
+			name,
+			body,
+			prerelease,
+			latest
+		})}`
+	);
+	const {data, status} = await github.rest.repos.createRelease({
+		owner,
+		repo,
+		tag_name: tag,
+		name,
+		body,
+		draft: false,
+		prerelease,
+		make_latest: latest
+	});
+
+	if (status !== 201) {
+		throw new Error(
+			`Failed to create release ${tag}: ${JSON.stringify(data)}`
+		);
+	}
+
+	return data.id;
+}
+
+async function uploadAssets(
+	inputs: InputsType,
+	releaseId: number
+): Promise<void> {
+	const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/');
+	const files = await glob(join(inputs.package.output, '*'));
+
+	info(`Uploading ${files.length} assets to release ${releaseId}`);
+	let uploaded = 0;
+	for (const file of files) {
+		for (const attempt of [1, 2, 3]) {
+			try {
+				if (attempt > 1) {
+					info(`Retrying upload of ${file} (attempt ${attempt})`);
+				} else {
+					info(`Uploading ${file}`);
+				}
+
+				await inputs.credentials.github.rest.repos.uploadReleaseAsset({
+					owner,
+					repo,
+					release_id: releaseId,
+					name: basename(file),
+					data: `@${file}`
+				});
+
+				uploaded += 1;
+				info(`Uploaded ${file}`);
+				break;
+			} catch (err: unknown) {
+				if (attempt < 3) {
+					const delay = 10 + Math.ceil(Math.random() * 200);
+					warning(
+						`Failed to upload ${file}: ${err}, retrying after ${delay}ms`
+					);
+					await new Promise(resolve => setTimeout(resolve, delay));
+				} else {
+					error(`Failed to upload ${file}: ${err}, skipping`);
+				}
+			}
+		}
+	}
+	info(`Uploaded ${uploaded}/${files.length} assets to release ${releaseId}`);
 }
